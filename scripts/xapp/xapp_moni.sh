5ec76ba97d8771c835bb1a9d588d84fbd1f12134
Van Tuan Ngo <vantuan_ngo@sutd.edu.sg>
2025-04-03 13:47:57 +0800

    publish datalake for lite on RU + run xapp with sdk

---
+++ scripts/xapp/xapp_moni.sh
@@ -0,0 +1,6 @@
+SCRIPT_DIR=$(dirname "$0")
+export PYTHONPATH=$SCRIPT_DIR/../../openairinterface5g/openair2/E2AP/flexric/build/examples/xApp/python3:$PYTHONPATH
+export LD_LIBRARY_PATH=$SCRIPT_DIR/../../openairinterface5g/openair2/E2AP/flexric/build/examples/xApp/python3:$LD_LIBRARY_PATH
+export PATH=$SCRIPT_DIR/../../openairinterface5g/openair2/E2AP/flexric/build/examples/xApp/python3:$PATH
+
+python3 $SCRIPT_DIR/xapp_mac_rlc_pdcp_gtp_moni.py
