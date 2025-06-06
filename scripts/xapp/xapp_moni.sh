d11ff5a5aa8f6e649ca734610cc6da69c2282789
Van Tuan Ngo <vantuan_ngo@sutd.edu.sg>
2025-06-07 01:19:46 +0800

    update oai

--- scripts/xapp/xapp_moni.sh
+++ scripts/xapp/xapp_moni.sh
@@ -1,4 +1,8 @@
+#1/bin/bash
+
 SCRIPT_DIR=$(dirname "$0")
+echo "Running xApp monitoring script from $SCRIPT_DIR"
+
 export PYTHONPATH=$SCRIPT_DIR/../../openairinterface5g/openair2/E2AP/flexric/build/examples/xApp/python3:$PYTHONPATH
 export LD_LIBRARY_PATH=$SCRIPT_DIR/../../openairinterface5g/openair2/E2AP/flexric/build/examples/xApp/python3:$LD_LIBRARY_PATH
 export PATH=$SCRIPT_DIR/../../openairinterface5g/openair2/E2AP/flexric/build/examples/xApp/python3:$PATH
