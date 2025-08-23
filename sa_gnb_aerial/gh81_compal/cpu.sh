beda4c5b309e9376196192a44dd924fcc07cc8ca
Tuan V. Ngo <tuannv.email@gmail.com>
2025-08-23 04:30:43 +0000

    Add more RUs

---
+++ sa_gnb_aerial/gh81_compal/cpu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Setting CPU idle to 0"
+sudo cpupower idle-set -D 0
+
+echo "Setting CPU frequency to performance"
+for ((i=0;i<$(nproc);i++)); do sudo cpufreq-set -c $i -r -g performance; done
